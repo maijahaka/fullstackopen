7d4b83167e3521bd7702d45b38207537aad8daa2
Maija Haka <maija.haka@live.fi>
2020-12-02 20:20:18 +0200

    1.12: anekdootit step1

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ( { handleClick, text } ) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const randomIndex = () => (
+    Math.floor(Math.random() * 6)
+  )
+  
+  const [selected, setSelected] = useState(0)
+
+  const handleButtonClick = () => (
+      setSelected(randomIndex)
+  )
+  
+  return (
+    <>
+    <div>
+      {props.anecdotes[selected]}
+    </div>
+
+    <Button handleClick={handleButtonClick} text="next anecdote" />
+    </>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the efirst 90 percent of the develpoment time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
