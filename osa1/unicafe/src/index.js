40dd871ac90f446f2ebcb9c015b6d2eceffdb36d
Maija Haka <maija.haka@live.fi>
2020-12-02 15:01:58 +0200

    1.7: unicafe step2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -7,9 +7,34 @@ const Button = ({ handleClick, text }) => (
     </button>
 )
 
-const Statistics = ({ text, value }) => (
-    <p>{text} {value}</p>
+const Summary = ({ text1, value, text2 }) => (
+    <p>{text1} {value} {text2}</p>
 )
+
+const Statistics = ({ good, neutral, bad }) => {
+    const sum = (good, neutral, bad) => (
+        good + neutral + bad
+    )
+
+    const average = (good, neutral, bad) => (
+        (1 * good + 0 * neutral + (-1) * bad) / sum(good, neutral, bad)
+    )
+
+    const positive = (good, neutral, bad) => (
+        good / sum(good, neutral, bad) * 100
+    )
+    
+    return (
+        <div>
+            <Summary text1="good" value={good} />
+            <Summary text1="neutral" value={neutral} />
+            <Summary text1="bad" value={bad} />
+            <Summary text1="all" value={sum(good, neutral, bad)} />
+            <Summary text1="average" value={average(good, neutral, bad)} />
+            <Summary text1="positive" value={positive(good, neutral, bad)} text2="%" />
+        </div>
+    )
+}
 
 const App = () => {
     const [good, setGood] = useState(0)
@@ -40,11 +65,7 @@ const App = () => {
 
         <h1>statistics</h1>
 
-        <div>
-            <Statistics text="good" value={good} />
-            <Statistics text="neutral" value={neutral} />
-            <Statistics text="bad" value={bad} />
-        </div>
+        <Statistics good={good} neutral={neutral} bad={bad} />
 
         </>
     )
