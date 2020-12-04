fa5e3cec093a6415da8748c20da5160ba5fa4ff7
Maija Haka <maija.haka@live.fi>
2020-12-04 16:38:25 +0200

    2.1: kurssitiedot step6

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -20,46 +20,48 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
+      {props.course.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
     </div>
   )
 }
 
-const Total = (props) => {
-  return(
-    <p>Number of exercises {
-      props.course.parts[0].exercises + 
-      props.course.parts[1].exercises + 
-      props.course.parts[2].exercises}</p>
-  )
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course} />
+      <Content course={props.course} />
+    </>
+  )  
 }
 
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
-      name: 'Using props to pass data',
-      exercises: 7
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Course course={course} />
     </div>
   )
 }
