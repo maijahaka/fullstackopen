4d17b4577b7b00e1f459e77a9a9db70b26972a86
Maija Haka <maija.haka@live.fi>
2020-12-09 16:21:33 +0200

    2.15: puhelinluettelo step7

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -32,7 +32,12 @@ const App = () => {
                 number: newNumber
             }
 
-            setPersons(persons.concat(personObject))
+
+            axios
+                .post('http://localhost:3001/persons', personObject)
+                .then(response => 
+                    setPersons(persons.concat(response.data))    
+                )
         }
 
         setNewName('')
